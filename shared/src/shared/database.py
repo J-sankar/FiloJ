9b5706966b8a0ec0efb8638d20016b3a936a162e
Jayasankar Menon <jayasankarmenonv@gmail.com>
2026-06-17 21:00:10 +0530

    fix: Base declaration by inheriting from Declarative Base class

--- shared/src/shared/database.py
+++ shared/src/shared/database.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 from sqlalchemy.ext.asyncio import AsyncSession,create_async_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, DeclarativeBase, MappedAsDataclass
 from typing import AsyncGenerator
 
 load_dotenv()
@@ -16,7 +16,8 @@ AsyncSesssionLocal = sessionmaker(
     expire_on_commit=False
 )
 
-Base = declarative_base()
+class Base(DeclarativeBase):
+    pass
 
 
 async def get_session() -> AsyncGenerator[AsyncSession, None]:
