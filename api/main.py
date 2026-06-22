a9ff150430b6f8b9c4c6521fbb86466e6c28cc0a
Jayasankar Menon <jayasankarmenonv@gmail.com>
2026-06-22 21:32:50 +0530

    add: Entry to fille_metadata and link with job

--- api/main.py
+++ api/main.py
@@ -1,17 +1,19 @@
-from fastapi import FastAPI,UploadFile,  File, Depends
+from fastapi import FastAPI, UploadFile, File, Depends
 from fastapi.exceptions import HTTPException
 from contextlib import asynccontextmanager
 from shared.logger import get_logger
-from shared.database import get_session,engine, Base,AsyncSession
-from shared.models import (Job)
+from shared.database import get_session, engine, Base, AsyncSession
+from shared.models import Job, FileMetaData
 from shared.broker import BrokerClient
 from shared.storage import S3StorageAdapter
-from api.utils import allowed_file_type, get_file_type,ALLOWED_FILES
+from api.utils import allowed_file_type, get_file_type
 from sqlalchemy import select
 import hashlib
+
 logger = get_logger(__name__)
 storage = S3StorageAdapter()
 broker = BrokerClient()
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -25,21 +27,25 @@ async def lifespan(app: FastAPI):
     await broker.close()
     logger.info("Shutting down")
 
+
 app = FastAPI(title="FiloJ", lifespan=lifespan)
 
-@app.get('/health')
+
+@app.get("/health")
 async def health_check():
     return {"status": "healthy"}
 
 
 async def iterator(filename: str):
     with open(filename, "rb") as f:
-        while chunk := f.read(1024  * 1024):
-            yield chunk  
-    
+        while chunk := f.read(1024 * 1024):
+            yield chunk
+
 
 @app.post("/file/upload")
-async def upload_file(uploaded_file: UploadFile = File(...),db : AsyncSession =Depends(get_session)):
+async def upload_file(
+    uploaded_file: UploadFile = File(...), db: AsyncSession = Depends(get_session)
+):
     filename = uploaded_file.filename
     filetype = get_file_type(filename)
     if not allowed_file_type(filename):
@@ -52,40 +58,67 @@ async def upload_file(uploaded_file: UploadFile = File(...),db : AsyncSession =D
             m.update(chunk)
         file_hash = m.hexdigest()
         file_key = f"{file_hash}{filetype}"
-        job_res = await db.execute(select(Job).where(Job.upload_id == file_key))
+        job_res = await db.execute(
+            select(Job)
+            .join(Job.file)
+            .where(FileMetaData.upload_id == file_hash)
+            .order_by(Job.updated_at.desc())
+        )
         existing_job = job_res.scalars().first()
         if existing_job:
-            logger.info("File hash exists in db")
-            return {
-                "message": "File already exists",
-                "job_id": str(existing_job.id),
-                "status": existing_job.status
-            }
+            if existing_job.status in (
+                "scanning",
+                "processing",
+                "completed",
+                "pending",
+            ):
+                logger.info("File hash exists in db")
+                return {
+                    "message": "File already exists",
+                    "job_id": str(existing_job.id),
+                    "status": existing_job.status,
+                }
         await uploaded_file.seek(0)
         file_stream = uploaded_file.file
         await storage.upload_file(file_key=file_key, file_stream=file_stream)
         logger.info(f"File Uploaded to Storage | {filename}")
-        new_job = Job(upload_id=file_key, job_type="scan" )
-        db.add(new_job)
+        async with db.begin_nested():
+    
+    
+            file_metadata = FileMetaData(
+                type=filetype,
+                name=filename,
+                upload_id=file_hash,
+                storage_location=file_key
+            )   
+ 
+            new_job = Job(
+                file=file_metadata  
+            )
+            
+            db.add(file_metadata)
+            db.add(new_job)
         await db.commit()
-        job_payload = {
-            "job_id": str(new_job.id), "file_hash": file_key
+        job_payload = {"job_id": str(new_job.id), "file_hash": file_key}
+        await broker.publish(
+            exchange_name="work.tasks",
+            routing_key=f"task{filetype}.scan",
+            payload=job_payload,
+        )
+        new_log = {
+            "job_id": str(new_job.id),
+            "routing_key": f"task{filetype}.scan",
+            "service": "api",
+            "action": "file scheduled for scanning",
         }
-        await broker.publish(exchange_name="work.tasks", routing_key=f"task{filetype}.scan", payload=job_payload )
-        new_log = {"job_id":str(new_job.id),"routing_key":f"task{filetype}.scan","service":"api","action":"file scheduled for scanning"}
-        await broker.publish("system.events",routing_key="", payload=new_log )
-        logger.info(f"Scan job scheduled | job: {str(new_job.id)[:8]} | file: {filename}")
-        if ALLOWED_FILES.get(filetype) == "image":
-            new_image_job = Job(upload_id=file_key, job_type = "img_processing")
-            db.add(new_image_job)
-            await db.commit()
-            await broker.publish(exchange_name="work.tasks", routing_key=f"task{filetype}.process", payload={"job_id": str(new_image_job.id), "file_hash": file_hash} )
-            logger.info(f"Image Process job scheduled | job: {str(new_image_job.id)[:8]} | file: {filename}")
-            await broker.publish("system.events",routing_key="", payload= {"job_id":str(new_image_job.id),"routing_key":f"task{filetype}.process","service":"api","action":"image scheduled for processing"})
+        await broker.publish("system.events", routing_key="", payload=new_log)
+        logger.info(
+            f"Scan job scheduled | job: {str(new_job.id)[:8]} | file: {filename}"
+        )
         return {
             "message": "File uploaded and scheduled for processing",
             "job_id": str(new_job.id),
-            "upload_id": file_key
+            "upload_id": file_key,
         }
     except Exception as e:
         await db.rollback()
