6096d5e4ed3cc640329a3d716aa9a4cb8d0e72bd
Jayasankar Menon <jayasankarmenonv@gmail.com>
2026-07-03 21:53:55 +0530

    add: implement AuthServiceServicer for gRPC

--- auth_service/grpc/grpc_servicer.py
+++ auth_service/grpc/grpc_servicer.py
@@ -19,15 +19,15 @@ class AuthService(auth_pb2_grpc.AuthServiceServicer):
                 developer = await validate_api_key(api_key=request.api_key,db=db)
             except (InactiveDeveloperError,InvalidApiKeyError,InactiveApiKeyError) as e:
                 logger.warning(f"Auth failed: {str(e).lower()}")
-                return context.abort(
+                await context.abort(
                     grpc.StatusCode.UNAUTHENTICATED,
                     str(e).lower()
                 )
             except Exception as e:
                 logger.warning(f"Auth failed: {str(e).lower()}")
-                return context.abort(
+                await context.abort(
                     grpc.StatusCode.INTERNAL,
                     str(e).lower()
                 )
 
-        return auth_pb2.ValidateKeyResponse(valid=True, plan=developer.plan)
+        return auth_pb2.ValidateKeyResponse(valid=True, plan=developer.plan, developer_id=str(developer.id))
