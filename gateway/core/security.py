def881a9e4b90033576295233331482cbb6f2414
Jayasankar Menon <jayasankarmenonv@gmail.com>
2026-07-05 16:43:19 +0530

    refactor breakdown token and key functions to separate concerns

--- gateway/core/security.py
+++ gateway/core/security.py
@@ -1,16 +1,16 @@
-from gateway.grpc_clients.auth_client import AuthGrpcClient
 from fastapi import HTTPException, Request
 from jose import JWTError, jwt
 
 from gateway.core.config import settings
 from shared.logger import get_logger
-
+import hashlib
 logger  = get_logger(__name__)
 
 
-grpc_auth_client = AuthGrpcClient()
 
-def decode_token(request:Request) :
+
+
+def extract_bearer_headers(request: Request) -> str :
     headers = request.headers
     auth_headers = headers.get("Authorization")
     if not auth_headers:
@@ -20,6 +20,9 @@ def decode_token(request:Request) :
     if len(parts) != 2 or parts[0].lower() != "bearer":
         raise HTTPException(401, "Invalid Authorization header format")
     bearer_token = parts[1]
+    return bearer_token
+
+def decode_token(bearer_token:str) :
     try:
         token = jwt.decode(
             bearer_token,
@@ -35,19 +38,6 @@ def decode_token(request:Request) :
 
 
 
-async def verify_api_key(request:Request):
-    headers = request.headers
-    auth_headers = headers.get("Authorization")
-    if not auth_headers:
-        logger.warning("No auth headers found")
-        raise HTTPException(401,"No auth headers")
-    parts = auth_headers.split(" ")
-    if len(parts) != 2 or parts[0].lower() != "bearer":
-        raise HTTPException(401, "Invalid Authorization header format")
-    api_key = parts[1]
-    if not api_key:
-        raise HTTPException(401, "Api Key Missing")
-    response = await grpc_auth_client.validate_key(api_key)
 
-    return response 
-   
+def hash_key(api_key:str)->str:
+    return hashlib.sha256(api_key.encode()).hexdigest()
